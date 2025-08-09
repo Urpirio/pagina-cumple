30a80fc9fec462f8c81f4c2d5853fa6acb52dd5c
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-09 14:43:37 -0400

    Se agrego el hook de eestilos

---
+++ hook/useStyle.tsx
@@ -0,0 +1,42 @@
+import { color } from "motion";
+import { useEffect, useState } from "react";
+
+export const useStyle = () => {
+  const [BgColor, setBgColor] = useState<boolean>(false);
+  const [ContadorClickers,setContadorClickers] = useState<number>(1);
+  const [StatusDialog,setStatusDialog] = useState<boolean>(false)
+
+  const VarianColor = {
+    Color: {
+      scale: 0.5,
+    },
+    Color2: {
+      scale: 1,
+    },
+  };
+  const AnimTime = () => {
+    setInterval(() => {
+      if (BgColor) {
+        setBgColor(false);
+      } else {
+        setBgColor(true);
+      }
+    }, 500);
+  };
+
+  useEffect(()=>{
+
+  },[ContadorClickers])
+
+
+  return {
+    BgColor,
+    setBgColor,
+    VarianColor,
+    AnimTime,
+    setContadorClickers,
+    ContadorClickers,
+    StatusDialog,
+    setStatusDialog,
+  };
+};
