853058564107311b0be6cfca68249cf7553deaab
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-09 14:43:00 -0400

    Se agrego el componente que despliega la carta

---
+++ Components/DialogCarta.tsx
@@ -0,0 +1,24 @@
+import { Dialog } from "primereact/dialog";
+import { Dispatch, SetStateAction } from "react";
+
+
+export default function DialogCarta(D: {
+  StatusDialog: boolean;
+  setStatusDialog: Dispatch<SetStateAction<boolean>>;
+}) {
+    const {StatusDialog,setStatusDialog} = D;
+  return (
+    <Dialog
+      visible={StatusDialog}
+      className="h-full w-1/2 border bg-white  border-gray-300 shadow-2xl rounded-3xl"
+      maskClassName="backdrop-blur-xs"
+      closeIcon={true}
+      onHide={() =>{}}
+    >
+      <section className="bg-white h-full w-full rounded-3xl">
+
+
+      </section>
+    </Dialog>
+  );
+}
