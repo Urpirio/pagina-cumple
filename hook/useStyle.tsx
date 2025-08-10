5b3f6b6579a0d6e2ef9a9a86edb1fb9c7e49cf72
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-10 11:28:18 -0400

    Se agrego el cambio de imagenes en el hook de estilos

--- hook/useStyle.tsx
+++ hook/useStyle.tsx
@@ -1,19 +1,27 @@
 import { color } from "motion";
 import { useEffect, useState } from "react";
+import { motion, AnimatePresence } from "motion/react";
+import Image from "next/image";
+import Foto1 from "@/../public/Ivanna_6años.jpeg";
+import FotoMeses from "@/../public/Ivanna_8meses.jpeg";
+import Foto15 from "@/../public/Ivanna_15años.jpeg";
+import Foto19 from "@/../public/Ivanna_19años.jpeg";
 
 export const useStyle = () => {
   const [BgColor, setBgColor] = useState<boolean>(false);
   const [ContadorClickers, setContadorClickers] = useState<number>(1);
   const [StatusDialog, setStatusDialog] = useState<boolean>(false);
+  const [image, setImage] = useState<any>();
 
   const VarianColor = {
     Color: {
       scale: 0.5,
     },
     Color2: {
-      scale: 1,
+      scale: 0.7,
     },
   };
+
   const AnimTime = () => {
     setTimeout(() => {
       if (BgColor && ContadorClickers < 20) {
@@ -24,6 +32,61 @@ export const useStyle = () => {
     }, 500);
   };
 
+  const ChangeImg = () => {
+    if (ContadorClickers === 1) {
+      setImage(
+        <motion.article
+          animate={{ translateY: -80 }}
+          className="border border-gray-50 shadow-2xl p-2 bg-white  shadow-rose-500 rounded-full"
+        >
+          <Image
+            className="rounded-full w-20 h-20 object-cover"
+            src={FotoMeses}
+            alt=""
+          />
+        </motion.article>
+      );
+    } else if (ContadorClickers === 6) {
+      setImage(
+        <motion.article
+          animate={{ opacity: 1 }}
+          className="opacity-0 p-2 rounded-full bg-white"
+        >
+          <Image
+            className="rounded-full w-40 h-40 object-cover"
+            src={Foto1}
+            alt=""
+          />
+        </motion.article>
+      );
+    } else if (ContadorClickers === 15) {
+      setImage(
+        <motion.article
+          animate={{ opacity: 1 }}
+          className="opacity-0 p-2 rounded-full bg-white"
+        >
+          <Image
+            className="rounded-full w-60 h-60 object-cover"
+            src={Foto15}
+            alt=""
+          />
+        </motion.article>
+      );
+    } else if (ContadorClickers === 19) {
+      setImage(
+        <motion.article
+          animate={{ opacity: 1 }}
+          className="opacity-0 p-2 rounded-full bg-white"
+        >
+          <Image
+            className="rounded-full w-80 h-80 object-cover"
+            src={Foto19}
+            alt=""
+          />
+        </motion.article>
+      );
+    }
+  };
 
   return {
     BgColor,
@@ -34,5 +97,7 @@ export const useStyle = () => {
     ContadorClickers,
     StatusDialog,
     setStatusDialog,
+    ChangeImg,
+    image
   };
 };
