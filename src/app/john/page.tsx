f8557ddb055390f17bebd7605f1d3fee741e29be
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-08-11 08:34:45 -0400

    Update

--- src/app/john/page.tsx
+++ src/app/john/page.tsx
@@ -3,7 +3,7 @@ import DialogCarta from "../../../Components/Components - John/DialogCarta";
 export default function page() {
   return (
     <main className="h-screen w-full bg-linear-to-r from-violet-200 to-violet-500">
-      <audio src="/audio/ToyStorySong.mp3" autoPlay controls></audio>
+      <audio hidden src="/audio/ToyStorySong.mp3" autoPlay controls></audio>
         <DialogCarta/>
     </main>
   )
